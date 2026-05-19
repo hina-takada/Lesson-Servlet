385ebf99bba33b8cab6a852ac0a2ded270d81e72
hina-takada <hina.takada@arsnv.co.jp>
2026-05-19 17:40:03 +0900

    演習9

---
+++ src/main/java/chapter9/Redirect2.java
@@ -0,0 +1,24 @@
+package chapter9;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Redirect
+ */
+@WebServlet("/chapter9/redirect")
+public class Redirect2 extends HttpServlet {
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.sendRedirect("http://localhost:8080/Servlet/chapter9/done.jsp");
+	}
+
+}
