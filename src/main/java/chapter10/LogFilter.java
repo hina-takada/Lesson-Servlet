c4b834256c33e1593378393042fbe463e3247700
hina-takada <hina.takada@arsnv.co.jp>
2026-05-19 17:53:02 +0900

    演習10

---
+++ src/main/java/chapter10/LogFilter.java
@@ -0,0 +1,47 @@
+package chapter10;
+
+import java.io.IOException;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.FilterConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpFilter;
+import jakarta.servlet.http.HttpServletRequest;
+
+/**
+ * Servlet Filter implementation class LogFilter
+ */
+@WebFilter("/*")
+public class LogFilter extends HttpFilter implements Filter {
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		
+		
+		chain.doFilter(request, response);
+		HttpServletRequest sr = (HttpServletRequest)request;
+		System.out.println("[LOG]アクセスURL:" + sr.getRequestURI());
+		chain.doFilter(request, response);
+		
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
